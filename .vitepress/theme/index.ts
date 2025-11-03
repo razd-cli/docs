31b8a0ba217410f4a971ed7552a5cbff8e3f1a7b
Daniil G <dealenx@gmail.com>
2025-11-03 17:53:31 +0700

    Add yandexMetrika integration and update dependencies in theme

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,11 +3,24 @@ import DefaultTheme from "vitepress/theme";
 import "@catppuccin/vitepress/theme/mocha/mauve.css";
 import AsciinemaPlayer from "./components/AsciinemaPlayer.vue";
 import VideoPlayer from "./components/VideoPlayer.vue";
+import { yandexMetrika } from "@hywax/vitepress-yandex-metrika";
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component("AsciinemaPlayer", AsciinemaPlayer);
     app.component("VideoPlayer", VideoPlayer);
+
+    yandexMetrika(app, {
+      counter: {
+        id: 105096989,
+        initParams: {
+          webvisor: true,
+          clickmap: true,
+          trackLinks: true,
+          accurateTrackBounce: true,
+        },
+      },
+    });
   },
 };
